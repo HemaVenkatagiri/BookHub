d87baa3970963073a216424141ec086b3347c0e4
HemaVenkatagiri <Venkatagirihema@gmail.com>
2023-02-07 09:21:40 +0000

    solution

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import {Switch, Route, Redirect} from 'react-router-dom'
+import NotFound from './components/NotFound'
+import Login from './components/Login'
+import Home from './components/Home'
+import Bookshelves from './components/Bookshelves'
+import ProtectedRoute from './components/ProtectedRoute'
+import BookDetails from './components/BookDetails'
+
+import './App.css'
+
+const App = () => (
+  <Switch>
+    <Route exact path="/login" component={Login} />
+    <ProtectedRoute exact path="/" component={Home} />
+    <ProtectedRoute exact path="/shelf" component={Bookshelves} />
+    <ProtectedRoute exact path="/books/:id" component={BookDetails} />
+    <Route path="/not-found" component={NotFound} />
+    <Redirect to="not-found" />
+  </Switch>
+)
+
+export default App
